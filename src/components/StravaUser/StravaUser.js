a71dcc729e64dcd112295d946f952c3cc85b6bfb
David Dominguez <352985+ddominguez@users.noreply.github.com>
2021-04-25 16:58:11 -0400

    Convert StravaUser to function component

--- src/components/StravaUser/StravaUser.js
+++ src/components/StravaUser/StravaUser.js
@@ -1,23 +1,21 @@
-import React, {Component} from 'react';
+import React from 'react';
 import './StravaUser.css';
 
-class StravaUser extends Component {
-  render() {
-    const {firstname, lastname, profile} = this.props.user;
-    const fullname = `${firstname} ${lastname}`;
-    return (
-      <div className="strava-user">
-        <div className="image">
-          <img src={profile} alt={fullname} />
-        </div>
-        <div className="strava-user-info">
-          <div className="name">
-            {fullname} 
-          </div>
+const StravaUser = (props) => {
+  const { user } = props;
+  const fullname = `${user.firstname} ${user.lastname}`;
+  return (
+    <div className="strava-user">
+      <div className="image">
+        <img src={user.profile} alt={fullname} />
+      </div>
+      <div className="strava-user-info">
+        <div className="name">
+          {fullname}
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default StravaUser;
